fc6e7834ce46ae35387273407566bffe2c78a25f
Johan <johan@cobblestone-bridge.com>
2017-07-13 16:49:19 +0200

    Fixed issue 34
    Added columns to the checkbox fields and also added bootstrap for fixing this problem.

--- app/js/gui.js
+++ app/js/gui.js
@@ -175,6 +175,8 @@ var CheckBox = function(text, checked) {
   this.rootElement.appendChild(this.checkBoxElement)
   this.rootElement.appendChild(this.labelElement)
 
+  this.rootElement.setAttribute('class', 'col-sm-4 panel-div')
+
   // Register event handler to update our internal state
   this.checkBoxElement.addEventListener('change', this.onChangeHandler)
 }
