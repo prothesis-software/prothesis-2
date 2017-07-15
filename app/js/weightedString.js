1360e3a76e5ef50197313cffc4263e502824d86b
RagingGrim <armandconnor.duplooy@gmail.com>
2017-07-15 15:01:31 +0200

    Added basic weightedStrings; Still need something to move around strings inside the class itself.

---
+++ app/js/weightedString.js
@@ -0,0 +1,46 @@
+/** @module weightedString*/
+'use strict';
+
+module.exports = class weightedString {
+     constructor() {
+          this.weights = [];
+          this.values = [];
+     }
+
+     push(value, weight) {
+          this.weights.push(weight);
+          this.values.push(value);
+          this._order();
+     }
+
+     get(i) {
+          return {
+               value: this.values[i],
+               weight: this.weights[i]
+          };
+     }
+
+     _order() {
+          var size = this.values.length;
+          for (var i = 0; i < size - 1; i++) {
+               for (var j = 0; j < size - i - 1; j++) {
+                    if (this.weights[j] > this.weights[j + 1]) {
+                         var tmp = this.weights[j];
+                         this.weights[j] = this.weights[j + 1];
+                         this.weights[j + 1] = tmp;
+
+                         tmp = this.values[j];
+                         this.values[j] = this.values[j + 1];
+                         this.values[j + 1] = tmp;
+                    }
+               }
+          }
+     }
+
+     _print() {
+          var size = this.values.length;
+          for (var i = 0; i < size; i++) {
+               console.log(this.values[i]);
+          }
+     }
+}
