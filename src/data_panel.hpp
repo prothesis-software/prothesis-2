b3065a182b75970e9b1f73c48ded4bc43cc00a01
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-11-25 16:08:51 +0200

    Add header for Data Panel

--- src/data_panel.hpp
+++ src/data_panel.hpp
@@ -1,6 +1,15 @@
 #ifndef DATA_PANEL_HPP_
 #define DATA_PANEL_HPP_
 
-#include "include/cpptoml.h"
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
+
+#include "cpptoml.h"
+
+class DataPanel : public wxPanel {
+ public:
+};
 
 #endif  // DATA_PANEL_HPP_
