7dc27ec8fad8478b73b495895006eba19ec0f41b
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-12-28 21:22:18 +0200

    Cache and build linux only

--- build-wxwidgets.sh
+++ build-wxwidgets.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-mkdir -p build
-cd build
+cd ~
 
+###############
+# LINUX
+#######
 wget https://github.com/wxWidgets/wxWidgets/releases/download/v3.0.3/wxWidgets-3.0.3.tar.bz2
 tar -xf wxWidgets-3.0.3.tar.bz2
 
 cd wxWidgets-3.0.3
 
+#------------
+# GTK2
+#-----
 mkdir build-gtk2
 cd build-gtk2
-../configure --disable-unicode --with-gtk=2
+../configure --prefix=~/wxwidgets/gtk2 --disable-unicode --with-gtk=2
 make
-sudo make install
+make install
 
-cd ../
-mkdir build-msw-static
-cd build-msw-static
-../configure --prefix=$(pwd) --host=i686-w64-mingw32 --disable-unicode --with-msw --without-subdirs --disable-shared
-make
-cd ../../../
+# cd ../
+# mkdir build-msw-static
+# cd build-msw-static
+# ../configure --prefix=$(pwd) --host=i686-w64-mingw32 --disable-unicode --with-msw --without-subdirs --disable-shared
+# make
