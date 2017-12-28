fa0b59db8d28cb9cba88d30fd4f7fa4b26f71605
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2017-12-28 21:46:09 +0200

    Patch

--- build-wxwidgets.sh
+++ build-wxwidgets.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 cd $HOME
 
 ###############
@@ -10,14 +11,20 @@ tar -xf wxWidgets-3.0.3.tar.bz2
 
 cd wxWidgets-3.0.3
 
+# Patch
+patch include/wx/filefn.h $TRAVIS_BUILD_DIR/wxwidgets.patch
+
 #------------
 # GTK2
 #-----
-mkdir build-gtk2
-cd build-gtk2
-../configure --prefix=$HOME/wxwidgets/gtk2 --disable-unicode --with-gtk=2
-make
-make install
+
+if ! [ -d ~/wxwidgets/build-gtk2 ]; then
+    mkdir build-gtk2
+    cd build-gtk2
+    ../configure --prefix=$HOME/wxwidgets/gtk2 --disable-unicode --with-gtk=2
+    make
+    make install
+fi
 
 # cd ../
 # mkdir build-msw-static
