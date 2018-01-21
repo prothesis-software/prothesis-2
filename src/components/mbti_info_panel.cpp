7fda07d9f01630cbbfb0c9f40bb6953111d5821f
Evert Geldenhuys <evert.geldenhuys2@gmail.com>
2018-01-21 20:17:39 +0200

    Fix make file?

--- src/components/mbti_info_panel.cpp
+++ src/components/mbti_info_panel.cpp
@@ -1,2 +1,12 @@
 #include "mbti_info_panel.hpp"
 
+#include <memory>
+#include <string>
+#include <vector>
+
+MbtiInfoPanel::MbtiInfoPanel(wxWindow* parent, wxWindowID id,
+                             const wxPoint& pos, const wxSize& size,
+                             int64_t style)
+    : wxPanel(parent, id, pos, size, style) {
+  DoLayout();
+}
